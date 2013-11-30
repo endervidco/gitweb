ade98113f87e03a7f378aacade401aa1a5ccff58
Sinan YILDIZ <info@syildiz.com>
2013-11-30 16:15:20 +0200

    Sinan

--- blog/index.php
+++ blog/index.php
@@ -7,5 +7,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
+//Sinan
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
